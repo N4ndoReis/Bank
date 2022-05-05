4f6659c5d605a33d16adb35bccf35d806b5b9eab
N4ndoReis <luizfernandoreis2014@gmail.com>
2022-05-04 23:57:58 -0300

    full aplication

---
+++ 04-ByteBank/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _04_ByteBank
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            ContaCorrente contaDoBruno = new ContaCorrente();
+            contaDoBruno.titular = "Bruno";
+            Console.WriteLine(contaDoBruno.saldo);
+            bool resultadoSaque = contaDoBruno.Sacar(500);
+            Console.WriteLine(contaDoBruno.saldo);
+            Console.WriteLine(resultadoSaque);
+
+
+            contaDoBruno.Depositar(500);
+            Console.Write(contaDoBruno.saldo);
+
+            ContaCorrente contaDaGabriela = new ContaCorrente();
+
+            contaDaGabriela.titular = "Gabriela";
+
+            Console.WriteLine("Saldo do Bruno" + contaDoBruno.saldo);
+            Console.WriteLine("Saldo da Gabriela" + contaDaGabriela.saldo);
+
+            bool resultadoTransferencia = contaDoBruno.Transferir(2000, contaDaGabriela);
+
+            Console.WriteLine("Saldo do Bruno" + contaDoBruno.saldo); 
+            Console.WriteLine("Saldo da Gabriela" + contaDaGabriela.saldo);
+
+            Console.WriteLine("resultado transferência:" + resultadoTransferencia);
+            
+            contaDaGabriela.Transferir(100, contaDoBruno);
+
+            Console.WriteLine("Saldo do Bruno:" + contaDoBruno.saldo);
+            Console.WriteLine("Saldo da Gabriela:" + contaDaGabriela.saldo);
+            Console.ReadLine(); 
+        }
+    }
+}
